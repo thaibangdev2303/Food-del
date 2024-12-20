4e6aad9ab24b915c9dea4d07a1ce9fc2deb61cd1
Food-del <vothaibang230303@gmail.com>
2024-12-20 18:42:45 +0700

    init project

--- food-del/frontend/src/components/Navbar/Navbar.jsx
+++ food-del/frontend/src/components/Navbar/Navbar.jsx
@@ -21,10 +21,10 @@ const Navbar = ({ setShowLogin }) => {
     <div className='navbar'>
       <Link to='/'><img src={assets.logo} alt="Company Logo" className="logo" /></Link>
       <ul className='navbar-menu'>
-        <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
-        <Link to='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</Link>
-        <Link to='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</Link>
-        <Link to='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</Link>
+        <a href='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</a>
+        <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
+        <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</a>
+        <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</a>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="Search Icon" />
